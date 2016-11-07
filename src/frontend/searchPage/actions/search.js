df6d4acea122a30b6336cc99d242aab80245ed9d
jekku <jekripreclaroorlina@gmail.com>
2016-11-07 17:07:28 +0800

    Updates - Add browser env for NightmareJS on .jshintrc - Fix linter warnings before repushing

--- src/frontend/searchPage/actions/search.js
+++ src/frontend/searchPage/actions/search.js
@@ -1,34 +1,36 @@
 'use strict';
 
-import {default as config} from 'config/config';
-import {default as Nightmare} from 'nightmare';
+import {default as nightmareFactory} from 'lib/NightmareFactory';
+import {default as should} from 'should';
 
 const performSearchByButton = function searchByButton (done) {
-  Nightmare({show: true})
-    .goto('https://www.google.com')
+  nightmareFactory()
     .type('[name=q]', 'Maria Sharapova')
     .wait(1000)
     .evaluate( () => document.title)
     .end()
-    .then( (title) => {
-      title.should.be.exactly('Google Search - Maria Sharapova');
-      done();
+    .catch( (err) => { 
+      should.not.exist(err);
     })
-    .catch(() => done());
+    .then( (title) => {
+      title.should.be.exactly('maria sharapova - Google Search');
+      done();
+    });
 };
 
 const performEmptySearch = function emptySearch (done) {
-  Nightmare({show: true})
-    .goto('https://www.google.com')
+  nightmareFactory()
     .click('[name=btnK]')
     .wait(1000)
     .evaluate( () => document.title)
     .end()
+    .catch( (err) => {
+      should.not.exist(err);
+    })
     .then( (title) => {
       title.should.be.exactly('Google');
       done();
-    })
-    .catch(() => done());
+    });
 };
 
 export default {
