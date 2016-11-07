5dadb39443201260c4e12f4f7c4bf551d47d7acf
Jekri Preclaro Orlina <jekku@users.noreply.github.com>
2016-11-07 18:04:07 +0800

    Use Nightmare.js instead of Selenium (#2)
    * Updates
    - Use nightmarejs instead of selenium in routines
    
    * Updates
    - Remove irrelevant library
    
    * Updates
    - Add browser env for NightmareJS on .jshintrc
    - Fix linter warnings before repushing
    
    * Updates
    - Fix code quality and rejection hangups without done function

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,9 @@
 
 export default {
   siteUrl: 'https://www.google.com',
-  seleniumServerUrl: 'http://localhost:4444/wd/hub',
   rootIntegrationIDs: ['frontend'],
+  nightmareSettings: {
+    show: true
+  },
 };
 
--- src/frontend/searchPage/actions/search.js
+++ src/frontend/searchPage/actions/search.js
@@ -1,54 +1,27 @@
 'use strict';
 
-import {default as config} from 'config/config';
-import {default as createWebDriver} from 'lib/WebDriver';
-import {default as Selenium} from 'selenium-webdriver';
+import {default as nightmareFactory} from 'lib/NightmareFactory';
+import {default as should} from 'should';
 
-let driver;
-let searchBar;
-let searchButton;
-
-const initialize = function initialize () {
-  driver = createWebDriver();
-
-  const bindElements = function () {
-    searchBar = driver.findElement(
-      Selenium.By.name('q')
-    );
-
-    searchButton = driver.findElement(
-      Selenium.By.name('btnG')
-    );
-  };
-
-  return driver.get(config.siteUrl)
-    .then(bindElements);
-};
-
-const performSearchByButton = function searchByButton (done) {
-  initialize()
-    .then( () => {
-      searchBar.sendKeys('Maria Sharapova');
-      searchButton.click();
-      return driver.getTitle();
-    })
-    .then( result => {
-      result.should.be.exactly('Maria Sharapova - Google Search');
-      driver.quit();
-      done();
+const performSearchByButton = function searchByButton () {
+  return nightmareFactory()
+    .type('[name=q]', 'Maria Sharapova')
+    .wait(1000)
+    .evaluate( () => document.title)
+    .end()
+    .then( (title) => {
+      title.should.be.exactly('maria sharapova - Google Search');
     });
 };
 
-const performEmptySearch = function emptySearch (done) {
-  initialize()
-    .then( () => {
-      searchBar.sendKeys(Selenium.Key.ENTER);
-      return driver.getTitle();
-    })
-    .then( result => {
-      result.should.be.exactly('Google');
-      driver.quit();
-      done();
+const performEmptySearch = function emptySearch () {
+  return nightmareFactory()
+    .click('[name=btnK]')
+    .wait(1000)
+    .evaluate( () => document.title)
+    .end()
+    .then( (title) => {
+      title.should.be.exactly('Google');
     });
 };
 
