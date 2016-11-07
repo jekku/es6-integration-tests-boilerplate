6cf3c88b06401f87505853563acadda0a13b7e3c
jekku <jekripreclaroorlina@gmail.com>
2016-11-07 17:12:11 +0800

    Updates - Fix code quality and rejection hangups without done function

--- src/frontend/searchPage/actions/search.js
+++ src/frontend/searchPage/actions/search.js
@@ -3,33 +3,25 @@
 import {default as nightmareFactory} from 'lib/NightmareFactory';
 import {default as should} from 'should';
 
-const performSearchByButton = function searchByButton (done) {
-  nightmareFactory()
+const performSearchByButton = function searchByButton () {
+  return nightmareFactory()
     .type('[name=q]', 'Maria Sharapova')
     .wait(1000)
     .evaluate( () => document.title)
     .end()
-    .catch( (err) => { 
-      should.not.exist(err);
-    })
     .then( (title) => {
       title.should.be.exactly('maria sharapova - Google Search');
-      done();
     });
 };
 
-const performEmptySearch = function emptySearch (done) {
-  nightmareFactory()
+const performEmptySearch = function emptySearch () {
+  return nightmareFactory()
     .click('[name=btnK]')
     .wait(1000)
     .evaluate( () => document.title)
     .end()
-    .catch( (err) => {
-      should.not.exist(err);
-    })
     .then( (title) => {
       title.should.be.exactly('Google');
-      done();
     });
 };
 
